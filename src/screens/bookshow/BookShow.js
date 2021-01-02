80d61b7c8190bd124ed8f05056fc34af1d289cf3
pranjul29 <paras.paharia29@gmail.com>
2021-01-03 00:58:00 +0530

    Added 'BookShow' component and Book Show button on header on details page

---
+++ src/screens/bookshow/BookShow.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Header from '../../common/header/Header';
+import Typography from "@material-ui/core/Typography";
+import Home from "../home/Home";
+import './BookShow.css'
+
+class BookShow extends Component {
+
+    backToDetailsHandler = () => {
+        ReactDOM.render(<Home />, document.getElementById('root'));
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="bookShow">
+                    <Typography className="back" onClick={this.backToDetailsHandler}>
+                        &#60; Back to Movie Details
+                    </Typography>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BookShow;
