7af42d2b4aa5899c927303ba31ac1b396c5b3b65
pranjul29 <paras.paharia29@gmail.com>
2021-01-02 13:38:51 +0530

    Added logo

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import './Header.css';
+import logo from '../../assets/logo.svg';
 
 class Header extends Component {
     render() {
         return (
             <header className="app-header">
+                <img src={logo} className="app-logo" alt="logo" />git
                 <div className="login-button">
                     <Button variant="contained" color="default">
                         Login
