7ead8796459b09e889599eb79540d28dd0653329
pranjul29 <paras.paharia29@gmail.com>
2021-01-02 13:34:50 +0530

    Added ‘Login’ button on Header

---
+++ src/common/header/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Header.css';
+import Button from '@material-ui/core/Button';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="app-header">
+                <div className="login-button">
+                    <Button variant="contained" color="default">
+                        Login
+                    </Button>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
