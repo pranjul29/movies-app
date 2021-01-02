60b5bf5bf6dbadccc3ee4dd0cae2f6e2db9e123a
pranjul29 <paras.paharia29@gmail.com>
2021-01-03 03:18:20 +0530

    Refactored code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './common/common.css';
 import 'typeface-roboto';
 import reportWebVitals from './reportWebVitals';
-import Home from './screens/home/Home';
+import Controller from './screens/Controller';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<Controller />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
 import Typography from '@material-ui/core/Typography';
 import './Details.css';
-import Home from '../home/Home';
 import YouTube from 'react-youtube';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
+import { Link } from 'react-router-dom';
 
 class Details extends Component {
     constructor() {
@@ -47,13 +46,9 @@ class Details extends Component {
     componentWillMount() {
         let currentState = this.state;
         currentState.movie = moviesData.filter((mov) => {
-            return mov.id === this.props.movieId
+            return mov.id === this.props.match.params.id
         })[0];
         this.setState({ currentState });
-    }
-
-    backToHomeHandler = () => {
-        ReactDOM.render(<Home />, document.getElementById('root'));
     }
 
     artistClickHandler = (url) => {
@@ -87,10 +82,10 @@ class Details extends Component {
         }
         return (
             <div className="details">
-                <Header showBookShowButton="true" />
+                <Header id={this.props.match.params.id} showBookShowButton="true" />
                 <div className="back">
-                    <Typography onClick={this.backToHomeHandler}>
-                        &#60; Back to Home
+                    <Typography>
+                        <Link to="/">  &#60; Back to Home</Link>
                     </Typography>
                 </div>
                 <div className="flex-containerDetails">
