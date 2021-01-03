496b78cc25fc4f42a21439b355197035203dbd09
pranjul29 <paras.paharia29@gmail.com>
2021-01-03 14:21:19 +0530

    Integrated register functionality

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -116,7 +116,7 @@ class Home extends Component {
         const { classes } = this.props;
         return (
             <div>
-                <Header />
+                <Header baseUrl={this.props.baseUrl}/>
                 <div className={classes.upcomingMoviesHeading}>
                     <span>Upcoming Movies</span>
                 </div>
